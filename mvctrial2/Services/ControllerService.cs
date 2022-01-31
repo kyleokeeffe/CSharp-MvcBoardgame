ea85fdc321ffe4298217b3aa8157081bb41d2349
Kyle O'Keeffe <78436677+kyleokeeffe@users.noreply.github.com>
2022-01-31 16:03:36 -0500

    Changed ControllerService to nonstatic

--- mvctrial2/Services/ControllerService.cs
+++ mvctrial2/Services/ControllerService.cs
@@ -5,18 +5,22 @@ using mvctrial2.Controllers;
 
 namespace mvctrial2.Services
 {
-    public static class ControllerService
+    public class ControllerService
     {
      
-        public static readonly GameController GameCon;
-        public static readonly PieceController PieceCon;
-        public static readonly BoardController BoardCon;
+        private GameController _GameCon;
+        private PieceController _PieceCon;
+        private BoardController _BoardCon;
+        public PieceController PieceCon {get{return this._PieceCon;} set{this._PieceCon.value;}}
+        public GameController GameCon {get{return this._GameCon;} set{this._GameCon.value;}}
+        public BoardController BoardCon {get{return this._BoardCon;} set{this._BoardCon.value;}}
+        
 
        // public GameController GameCon { get { return this._GameCon; } set { this._GameCon = value; } }
        // public PieceController PieceCon { get { return this._PieceCon; } set { this._PieceCon = value; } }
       // public BoardController BoardCon { get { return this._BoardCon; } set { this._BoardCon = value; } }
 
-        static ControllerService()
+        public ControllerService()
         {
             GameCon = GameController.GetInstance();
             BoardCon = BoardController.GetInstance();
